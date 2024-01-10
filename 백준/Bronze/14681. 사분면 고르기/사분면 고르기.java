2dd8d40e9579766b2f5231a3ab4027b5166881dd
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-10 17:40:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -7,15 +7,19 @@ public class Main{
         int x = Integer.parseInt(br.readLine());
         int y = Integer.parseInt(br.readLine());
         
-        if( x < 0 && y < 0){
-            System.out.println(3);
-        } else if( x < 0 && y > 0){
-            System.out.println(2);
-        } else if( x > 0 && y < 0){
-            System.out.println(4);
+        if( x > 0 ){
+            if( y > 0 ){
+                System.out.println(1);
+            } else {
+                System.out.println(4);
+            }
         } else {
-            System.out.println(1);
+            if( y > 0 ){
+                System.out.println(2);
+            } else {
+                System.out.println(3);
+            }
         }
-
+        
 	}
 }
