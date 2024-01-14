c85c1e216e0459becaa5daabdbf2d67ef52ed513
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-15 05:48:09 +0900

    [Bronze I] Title: 단어 공부, Time: 296 ms, Memory: 21072 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,45 +1,41 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.StringBuilder;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
         
         String input = br.readLine().toUpperCase();
-        
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-
-        for(int i = 0; i<input.length(); i++){
-           map.put(input.charAt(i), map.getOrDefault(input.charAt(i), 0) +1 );
+        int[] alphabet = new int[90-64];
+        int idx = 0;
+        for( int i = 0; i<input.length(); i++){
+            idx = input.charAt(i);
+            alphabet[idx-65]++;
         }
 
         int max = 0;
-        char result = 'a';
-        for( int i = 0; i<input.length(); i++){
-            if( map.get(input.charAt(i)) > max ){
-              max = map.get(input.charAt(i));
-              result = input.charAt(i);
+        int dup = 0;
+        int resultIdx = 0;
+        for( int i = 0; i<alphabet.length; i++){
+            if( alphabet[i] > max){
+                max = alphabet[i];
+                resultIdx = i;
             }
         }
 
-        Set<Character> set = new HashSet<Character>();
-        for( int i = 0; i<input.length(); i++){
-            if( map.get(input.charAt(i)) == max ){
-                set.add(input.charAt(i));
+        for( int i = 0; i<alphabet.length; i++){
+            if( alphabet[i] == max ){
+                dup++;
             }
         }
 
-        if( set.size() > 1){
+        if( dup > 1){
             System.out.print("?");
-        } else {
-            System.out.print(result);
+        } else{
+            System.out.print((char)(resultIdx + 65));
         }
+
 
         br.close();
 	}
