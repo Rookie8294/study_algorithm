ec5a0b8fb9039195098a24700376ee80deb90b3f
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-14 16:55:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+       	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        
+        System.out.print(br.readLine().codePointAt(0));
+
+        br.close();
+	}
+}
