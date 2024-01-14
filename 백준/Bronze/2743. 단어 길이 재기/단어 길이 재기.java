f979bc9fe998ea801e4c8715d5ca570d214fec7b
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-14 16:44:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.lang.StringBuilder;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+       	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        System.out.print(br.readLine().length());
+        
+        br.close();
+	}
+}
