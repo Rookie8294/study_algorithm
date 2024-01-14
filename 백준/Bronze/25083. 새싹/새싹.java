44aa665b3898b43bdae681262ab742b2e404e522
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-15 03:30:11 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,15 @@
+import java.lang.StringBuilder;
+public class Main{
+	public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |");
+
+        System.out.print(sb);
+	}
+}
