094e685272d2700a00312a681c95c384c78b68fd
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-17 00:38:55 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,50 +1,36 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
         
-        Set<Character> set = new HashSet<Character>();
-
         int cnt = Integer.parseInt(br.readLine());
         int result = 0;
-        int minus = 0;
         String input = "";
-        List<Character> list = new ArrayList<Character>();
+        boolean[] visited;
+
         for( int i = 0; i<cnt; i++){
             input = br.readLine();
-
-            for( int j = 0; j<input.length(); j++){
-                if( j == input.length()-1){
-                    list.add(input.charAt(j));
-                    break;
-                }
-                if( (j+1) < input.length() && input.charAt(j) == input.charAt(j+1)){
+            visited = new boolean[30];
+            result++;
+            visited[input.charAt(0) - 'a'] = true;
+            for( int j = 1; j<input.length(); j++){
+                if( input.charAt(j) == input.charAt(j-1)){
                     continue;
-                } else {
-                    list.add(input.charAt(j));
                 }
-            }
-
-            for (int k = 0; k< list.size(); k++) {
-                if (!set.add(list.get(k))) {
-                    minus++;
+                if( visited[input.charAt(j) - 'a']){
+                    result--;
                     break;
                 }
+
+                visited[input.charAt(j) - 'a'] = true;
             }
-            list.clear();
-            set.clear();
+
         }
 
-        System.out.print(cnt - minus);
-        
-
+        System.out.print(result);
         br.close();
 	}
 }
