fdcc4322b36cbedeff33365cf198ff911ec6bd6a
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-23 13:11:10 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 124 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+       	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        
+        long square = Integer.parseInt(br.readLine());
+        System.out.println( 4 * square);
+        
+        
+
+        br.close();
+	}
+}
