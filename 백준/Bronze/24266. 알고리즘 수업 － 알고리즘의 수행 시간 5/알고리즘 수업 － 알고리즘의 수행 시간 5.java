421f7d957898b2188ffb705fa7254d2bbc1ab5ae
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-24 12:53:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.lang.StringBuilder;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+       	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+        
+        Long n = Long.parseLong(br.readLine());
+        
+        System.out.println(n*n*n);
+        System.out.print(3);
+
+        br.close();
+	}
+}
