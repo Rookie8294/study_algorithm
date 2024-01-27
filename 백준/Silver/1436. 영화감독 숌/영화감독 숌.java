a55c54fa8f01b0de23467a49fdfbba095753ced5
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-27 14:44:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 304 ms, Memory: 86876 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,40 +1,22 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.lang.StringBuilder;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        StringTokenizer st;
         
         int target = Integer.parseInt(br.readLine());
-        int compare = 0;
-
-        Long num = 666L;
-        int leng = 0;
-        String strNum = "";
-        while(true){
-            strNum = String.valueOf(num);
-            leng = strNum.length();
-            int cnt = 0;
-            for( int i = 0; i<leng; i++){
-                if( strNum.charAt(i) == '6' ){
-                    cnt++;
-                } else if( strNum.charAt(i) != '6' && cnt < 3){
-                    cnt = 0;
-                }
-            }
-            if( cnt >= 3 ){
-                compare++;
-            }
-            if( compare == target){
-                break;
-            }
+        int num = 666;
+        int count = 1;
+        
+        while(target != count){
             num++;
+            if( String.valueOf(num).contains("666")){
+                count++;
+            }
         }
-
+        
         System.out.println(num);
 
         br.close();
