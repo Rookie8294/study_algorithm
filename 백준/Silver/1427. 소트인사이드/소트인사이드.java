993f49bb6efda5d77ef465a021480bfa63abccd0
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-01-30 11:08:59 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -8,29 +8,24 @@ public class Main{
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         
-        String[] target = br.readLine().split("");
-        int[] arr = new int[target.length];
+        //      카운팅 정렬
+        String target = br.readLine();
+        int[] countArr = new int[10];
 
-        for(int i = 0; i<target.length; i++){
-            arr[i] = Integer.parseInt(target[i]);
+        for (int i = 0; i < target.length() ; i++) {
+            countArr[target.charAt(i) - '0']++;
         }
 
-        for( int i = 0; i<arr.length-1; i++){
-            for( int j = i+1; j<arr.length; j++){
-                int temp = 0;
-                if( arr[i] < arr[j]){
-                    temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                }
+
+        for( int i = 9; i>=0 ; i--){
+
+            while (countArr[i] > 0){
+                sb.append(i);
+                countArr[i]--;
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
-        }
-        
-        System.out.print(sb);
+        System.out.println(sb);
 
         br.close();
 	}
