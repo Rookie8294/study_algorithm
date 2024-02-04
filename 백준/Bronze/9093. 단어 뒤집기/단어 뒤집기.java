6cf68e498390ff7a1ff7e6da072efae703c6e114
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-02-04 20:10:59 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 492 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -2,25 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.StringBuilder;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String... args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
         
         for( int k = 0; k<N; k++){
-            String[] arr = br.readLine().split(" ");
-		    for(int i=0; i<arr.length; i++) {
-			    for( int j = arr[i].length()-1; j>=0; j--) {
-				    sb.append(arr[i].charAt(j));
-			    }//end for
-			    sb.append(" ");
-		    }//end for
-            sb.append("\n");
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while( st.hasMoreTokens() ){
+                StringBuilder sb = new StringBuilder(st.nextToken());
+                result.append(sb.reverse()).append(" ");
+            }
+		    result.append("\n");
         }//end for
         
-        System.out.print(sb);
+        System.out.print(result);
     }//main
 }//class
