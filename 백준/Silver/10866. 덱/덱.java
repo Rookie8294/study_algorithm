9d945a5719f54f72b53be13d90d1270e3b143789
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-02-08 12:15:42 +0900

    [Silver IV] Title: 덱, Time: 196 ms, Memory: 18704 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Deque;
-import java.util.ArrayDeque;
 import java.lang.StringBuilder;
 
 public class Main{
@@ -12,62 +10,159 @@ public class Main{
 	    StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         
-        Deque<Integer> deque = new ArrayDeque<Integer>();
+        Deque deque = new Deque();
 
         int N = Integer.parseInt(br.readLine());
-
 
         for( int i = 0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             switch (st.nextToken()){
                 case "push_back" :
-                    deque.addLast(Integer.parseInt(st.nextToken()));
+                    deque.push_back(Integer.parseInt(st.nextToken()));
                     break;
                 case "push_front" :
-                    deque.addFirst(Integer.parseInt(st.nextToken()));
+                    deque.push_front(Integer.parseInt(st.nextToken()));
                     break;
                 case "front" :
-                    if( deque.isEmpty()){
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(deque.peekFirst()).append("\n");
-                    }
+                    sb.append(deque.front()).append("\n");
                     break;
                 case "back" :
-                    if( deque.isEmpty()){
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(deque.peekLast()).append("\n");
-                    }
+                    sb.append(deque.back()).append("\n");
                     break;
                 case "size" :
-                    sb.append(deque.size()).append("\n");
+                    sb.append(deque.getSize()).append("\n");
                     break;
                 case "empty" :
-                    if(deque.isEmpty()){
-                        sb.append(1).append("\n");
-                    } else {
-                        sb.append(0).append("\n");
-                    }
+                    sb.append(deque.empty()).append("\n");
                     break;
                 case "pop_front" :
-                    if(deque.isEmpty()){
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(deque.pollFirst()).append("\n");
-                    }
+                    sb.append(deque.popFront()).append("\n");
                     break;
                 case "pop_back":
-                    if(deque.isEmpty()){
-                        sb.append(-1).append("\n");
-                    } else {
-                        sb.append(deque.pollLast()).append("\n");
-                    }
+                    sb.append(deque.popBack()).append("\n");
                     break;
             }
         }
 
-        br.close();
         System.out.println(sb);
+        br.close();
 	}
+    
+    public static class Deque {
+
+        private DoubleEndedQueueNode front;
+        private DoubleEndedQueueNode rear;
+        private int size;
+
+        public Deque() {
+            this.front = null;
+            this.rear = null;
+            this.size = 0;
+        }
+
+        public int empty(){
+            if( front == null ){
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        public void push_front(int num){
+            DoubleEndedQueueNode currentNode = new DoubleEndedQueueNode(num);
+            if(empty() == 1){
+                front = currentNode;
+                rear = currentNode;
+            } else {
+                front.frontLink = currentNode;
+                currentNode.rearLink = front;
+                front = currentNode;
+            }
+            size++;
+        }
+
+        public void push_back(int num){
+            DoubleEndedQueueNode currentNode = new DoubleEndedQueueNode(num);
+            if( empty() == 1 ){
+                front = currentNode;
+                rear = currentNode;
+            } else {
+                rear.rearLink = currentNode;
+                currentNode.frontLink = rear;
+                rear = currentNode;
+            }
+            size++;
+        }
+
+        public int popFront(){
+            if( empty() == 1 ){
+                return -1;
+            } else {
+                int num = front.getData();
+                if( size == 1 ){
+                    front = null;
+                    rear = null;
+                } else {
+                    front = front.rearLink;
+                    front.frontLink = null;
+                }
+                size--;
+                return num;
+            }
+        }
+
+        public int popBack(){
+            if( empty() == 1 ){
+                return -1;
+            } else {
+                int num = rear.getData();
+                if( size == 1 ){
+                    front = null;
+                    rear = null;
+                } else {
+                    rear = rear.frontLink;
+                    rear.rearLink = null;
+                }
+                size--;
+                return num;
+            }
+        }
+
+        public int front(){
+            if( empty() == 1 ){
+                return -1;
+            } else {
+                return front.getData();
+            }
+        }
+
+        public int back(){
+            if( empty() == 1 ){
+                return -1;
+            } else {
+                return rear.getData();
+            }
+        }
+
+        public int getSize(){
+            return this.size;
+        }
+    }
+
+    public static class DoubleEndedQueueNode {
+        private int data;
+        public DoubleEndedQueueNode frontLink;
+        public DoubleEndedQueueNode rearLink;
+
+        public DoubleEndedQueueNode(int data) {
+            this.data = data;
+            this.frontLink = null;
+            this.rearLink = null;
+        }
+
+        public int getData(){
+            return this.data;
+        }
+
+    }
 }
