dcc70e4ed3c27bc8d3e33b4e04a15e447126ba6a
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-02-15 12:03:33 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver V/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver V/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -3,31 +3,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.lang.StringBuilder;
-import java.math.BigInteger;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 	    StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-
+        
         int N = Integer.parseInt(br.readLine());
-        BigInteger factorial = BigInteger.ONE;
-        for( int i = 1; i<=N; i++){
-            factorial = factorial.multiply(BigInteger.valueOf(i));
-        }
-
-        String target = String.valueOf(factorial);
-
         int cnt = 0;
-        for(int i = target.length()-1; 0<=i; i-- ){
-            char c = target.charAt(i);
-            if( c == '0'){
-                cnt++;
-            } else{
-                break;
-            }
+        
+        while( N >= 5){
+            cnt += N/5;
+            N /= 5;
         }
+    
         System.out.print(cnt);
         br.close();
 	}
