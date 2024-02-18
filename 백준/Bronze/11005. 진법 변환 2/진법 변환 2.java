9c694b50db4431d50ee123ab0f63e134dbad1b1c
Yong Seok Jang <98216156+Rookie8294@users.noreply.github.com>
2024-02-18 12:11:10 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,44 +1,40 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 import java.lang.StringBuilder;
 
 public class Main{
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+	    StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        String[] line = br.readLine().split(" ");
-        int input = Integer.parseInt(line[0]);
-        int decimalSys =  Integer.parseInt(line[1]);
-        String result = "";
-        boolean flag = true;
-        int target = 0;
-        while( flag ){
-            target = input%decimalSys;
-            if( input / decimalSys == 0 ){
-                flag = false;
-            } else {
-                input = input / decimalSys;
-            }
-
-            if( target > 9 ){
-
-                result += Character.toString( target + 55);
-            } else {
-                result += String.valueOf(target);
-            }
-        }
-
-        for( int i = 1; i<result.length()+1; i++){
-            sb.append(result.charAt(result.length()-i));
-        }
-
-        System.out.println(sb);
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
         
-
+        System.out.print(base(N, B));
+        
         br.close();
 	}
+    
+    public static String base(int num, int b){
+        StringBuilder sb = new StringBuilder();
+        while (num >= b){
+            int target = num%b;
+            if( target < 10 ){
+                sb.append(target);
+            } else {
+                sb.append((char) (target + 55));
+            }
+            num /= b;
+        }
 
-
+        if( num < 10 ){
+            sb.append(num);
+        } else {
+            sb.append((char) (num+55));
+        }
+        return sb.reverse().toString();
+    }
 }
