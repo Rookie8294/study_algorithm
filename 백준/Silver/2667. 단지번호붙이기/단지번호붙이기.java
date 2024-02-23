965dbebfb2cb97c04f59d79658302cb99bc9b0c9
Jang Yong Seok <98216156+Rookie8294@users.noreply.github.com>
2024-02-23 20:18:58 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 import java.lang.StringBuilder;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Stack;
 
 public class Main{
 
@@ -13,18 +13,20 @@ public class Main{
     static boolean[][] chk;
     static int[][] map;
     static int cnt, n;
+    static Stack<int[]> stack;
     
 	public static void main(String[] args) throws IOException {
        	BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
         
         n = Integer.parseInt(br.readLine());
         map = new int[n][n];
         chk = new boolean[n][n];
         result = new ArrayList<>();
+        stack = new Stack<>();
         cnt = 1;
 
+		// 입력받은 지도(행열)를 탐색하기위해 이차원배열에 추가해줍니다.
         for( int i = 0; i<n; i++){
             String[] arr = br.readLine().split("");
             for( int j = 0; j<n; j++){
@@ -34,14 +36,15 @@ public class Main{
 
         for( int i = 0; i<n; i++){
             for( int j = 0; j<n; j++){
-                if( map[i][j] == 1 && !chk[i][j] ){
-                    dfs(i, j); // dfs 호출
+                if( map[i][j] == 1 && !chk[i][j]){
+                    dfsUseStack(i, j);
                     result.add(cnt);
-                    cnt = 1;
+                    cnt=1;
                 }
             }
-        }    
-        Collections.sort(result);
+        }   
+        
+        Collections.sort(result); // 오름차순 정렬
         sb.append(result.size()).append("\n");
         for( int i : result ){
             sb.append(i).append("\n");
@@ -50,20 +53,27 @@ public class Main{
         System.out.print(sb);
 	}
     
-    static void dfs(int y, int x){
+    static void dfsUseStack(int y, int x){
         chk[y][x] = true;
+        stack.push(new int[]{y, x});
         int[] dy = {0,1,0,-1};
         int[] dx = {1,0,-1,0};
-        for( int i = 0; i<4; i++){ // 4방향의 노드를 모두 탐색
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if( ny >= 0 && nx >= 0 && ny < n && nx < n ){ //인접한 노드가 없다면 탐색하지 않습니다.
-                if( map[ny][nx] == 1 && chk[ny][nx] == false){
-                    cnt++;
-                    dfs(ny, nx);
+
+        while (!stack.isEmpty()){
+            int[] arr = stack.pop();
+            for( int i = 0; i<4; i++){
+                int ny = arr[0] + dy[i];
+                int nx = arr[1] + dx[i];
+                if( ny >= 0 && nx >= 0 && ny < n && nx < n ){ //하지만 옆 노드가 없다면 탐색하지 않도록 조건
+                    if( map[ny][nx] == 1 && chk[ny][nx] == false){
+                        cnt++;
+                        chk[ny][nx] = true;
+                        stack.push(new int[]{ny, nx});
+                    }
                 }
             }
         }
+
     }
     
 }
