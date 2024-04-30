e1c2b0f1f6b5ff591dd57a88783ef1b671ccaf6f
Jang Yong Seok <98216156+Rookie8294@users.noreply.github.com>
2024-04-30 12:31:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1488 ms, Memory: 123676 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,60 +5,60 @@ import java.lang.StringBuilder;
 import java.util.*;
 
 public class Main{
-    static int[] searchArr, rangeArr, originArr;
+    static int[] rangeArr;
     public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 	
         int n = Integer.parseInt(br.readLine());
-        searchArr = new int[n];
+        rangeArr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<n; i++){
-            searchArr[i] = Integer.parseInt(st.nextToken());
-        }
-        int m = Integer.parseInt(br.readLine());
-        originArr = new int[m];
-        rangeArr = new int[m];
-        st = new StringTokenizer(br.readLine());
-        for( int i = 0; i<m; i++){
+
+        for( int i = 0; i<n; i++){
             rangeArr[i] = Integer.parseInt(st.nextToken());
-            originArr[i] = rangeArr[i];
         }
+
         Arrays.sort(rangeArr);
+        int m = Integer.parseInt(br.readLine());
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for( int i : searchArr ){
-            if( bSearch(i) ) {
-                map.put(i, map.getOrDefault(i, 0) + 1);
-            }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<m; i++){
+            int key = Integer.parseInt(st.nextToken());
+
+            sb.append(upperBound(key) - lowerBound(key)).append(" ");
         }
 
-        for( int i : originArr ){
-            sb.append(map.getOrDefault(i, 0)).append(" ");
-        }
-        
         System.out.print(sb);
 
     }
     
-    static boolean bSearch(int num){
+    static int upperBound(int key){
+        int lo = 0;
+        int hi = rangeArr.length;
 
-        int left = 0;
-        int right = rangeArr.length-1;
+        while (lo < hi){
+            int mid = (lo + hi) / 2;
 
-        while (left <= right){
-            int mid = (right + left) / 2;
-
-            if( rangeArr[mid] > num ){
-                right = mid - 1;
-            }else if(rangeArr[mid] < num){
-                left = mid + 1;
-            } else {
-                return true;
-            }
+            if( key < rangeArr[mid]) hi = mid;
+            else lo = mid+1;
         }
 
-        return false;
+        return lo;
+    }
+
+    static int lowerBound(int key){
+        int lo = 0;
+        int hi = rangeArr.length;
+
+        while (lo < hi){
+            int mid = (lo + hi) / 2;
+
+            if( key <= rangeArr[mid]) hi = mid;
+            else lo = mid +1;
+
+        }
+
+        return lo;
     }
 
 }
