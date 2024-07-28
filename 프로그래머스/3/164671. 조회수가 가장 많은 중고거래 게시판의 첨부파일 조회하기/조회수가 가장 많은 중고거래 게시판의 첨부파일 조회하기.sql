3fe783b894b76b56f1242187a2f9fe5ff0a0e239
Jang Yong Seok <98216156+Rookie8294@users.noreply.github.com>
2024-07-28 19:49:54 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,6 +1,6 @@
-
-select '/home/grep/src/' || ugf.board_id || '/' || file_id || file_name || file_ext as file_path
-from used_goods_file ugf, (select board_id, rank() over(order by views desc) ranking
-from used_goods_board) ugb
-where ugf.board_id = ugb.board_id and ugb.ranking = 1
+select '/home/grep/src/' || board_id || '/' || file_id || file_name || file_ext as file_path
+from used_goods_file
+where board_id = (select board_id from used_goods_board
+                     order by views desc
+                     fetch first 1 row only)
 order by file_id desc
