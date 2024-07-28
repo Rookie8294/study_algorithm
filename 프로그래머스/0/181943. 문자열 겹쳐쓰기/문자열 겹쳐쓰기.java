06f5effe9f5f19f2dfd69c4bb2eb3475164d7355
Jang Yong Seok <98216156+Rookie8294@users.noreply.github.com>
2024-07-28 21:13:36 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 18.24 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,22 +1,10 @@
-import java.lang.StringBuilder;
-
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder sb = new StringBuilder();
-        int middleLength = overwrite_string.length();
-        int myStringLength = my_string.length();
         String answer = "";
         
-        //첫 문자열 입력
-        for( int i = 0; i<s; i++) sb.append(my_string.charAt(i));
-        //중간 문자열 입력
-        for( int i = 0; i<middleLength; i++) sb.append(overwrite_string.charAt(i));
-        //마지막 문자열 입력
-        for( int i = middleLength+s; i<myStringLength; i++) sb.append(my_string.charAt(i));
+        answer = my_string.substring(0, s) + overwrite_string + 
+            my_string.substring(overwrite_string.length()+s, my_string.length());
         
-        
-        return sb.toString();
-        
-        
+        return answer;
     }
 }
