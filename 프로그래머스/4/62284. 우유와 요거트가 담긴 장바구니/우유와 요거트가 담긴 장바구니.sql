f9b4a87e3acc53d33e2fc9776bd50ffe8b45ae3f
Jang Yong Seok <98216156+Rookie8294@users.noreply.github.com>
2024-07-30 17:49:34 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,8 +1,7 @@
 -- 코드를 입력하세요
 select cart_id
-from( SELECT cart_id, listagg(name, ' ') within group(order by name) names
 from cart_products
-group by cart_id )
-where names like '%Yogurt%' and names like '%Milk%'
+where name = 'Yogurt' or name = 'Milk'
+group by cart_id
+having count(distinct name) >= 2
 order by cart_id
-
